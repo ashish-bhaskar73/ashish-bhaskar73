c7c95fc3d9225ec419cbbd2858c0a9d11e3bc471
ashish-bhaskar73 <88917747+ashish-bhaskar73@users.noreply.github.com>
2022-05-13 14:06:38 +0530

    Add files via upload

---
+++ power_sum.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+       int n;
+       cin>>n;
+       vector
+       int a[10];
+       for(int i=0;i<n;i++){
+           cin>>a[i];
+       } 
+       int temp=0;
+       for(int i=0;i<n;i++){
+           temp=temp+arr[i];
+       }
+       for(int i=0;i<n;i++){
+       if(temp==pow(2,i)){
+           cout<<"0"<<endl;
+       }
+       }
+    }
+}
